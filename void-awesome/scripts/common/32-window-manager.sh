af7c9a141764f68c64827e14e0cf5dc20c59b23b
mrcapivaro <guilhermepachecobatista@gmail.com>
2024-10-04 17:16:54 -0300

    update

--- void-awesome/scripts/common/32-window-manager.sh
+++ void-awesome/scripts/common/32-window-manager.sh
@@ -5,5 +5,9 @@ echo "[*] Setup Window Manager(and compositor)"
 sudo xbps-install -y awesome picom
 # ensure that the awesome wm starts with dbus-run-yession
 sudo sed -i \
-	'/^Exec=.*dbus-run-session/!s/\(^Exec=\)\(.*\)/\1dbus-run-session \2/' \
-	/usr/share/xsessions/awesome.desktop
+    '/^Exec=.*dbus-run-session/!s/\(^Exec=\)\(.*\)/\1dbus-run-session \2/' \
+    /usr/share/xsessions/awesome.desktop
+# ensure that a installed xfce polkit does not start with awesome
+sudo sed -i \
+    '/^OnlyShowIn=.*/!s/^NotShowIn=.*/OnlyShowIn=Xfce/' \
+    /etc/xdg/autostart/xfce-polkit.desktop
