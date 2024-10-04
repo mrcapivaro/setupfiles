d3b8b44f9bad57c3bd04ba060aaca1ccb664edf3
mrcapivaro <guilhermepachecobatista@gmail.com>
2024-10-04 15:27:26 -0300

    update

---
+++ void-awesome/scripts/laptop/70-touchpad.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+set -e
+echo "[*] Setup Touchpad"
+
+# https://wiki.archlinux.org/title/Touchpad_Synaptics
+sudo mkdir -p /etc/X11/xorg.conf.d
+sudo tee "/etc/X11/xorg.conf.d/76-synaptics.conf" >/dev/null <<EOF
+Section "InputClass"
+    Identifier "touchpad"
+    Driver "synaptics"
+    MatchIsTouchpad "on"
+        Option "TapButton1" "1"
+        Option "TapButton2" "3"
+        Option "TapButton3" "2"
+        Option "VertEdgeScroll" "on"
+        Option "VertTwoFingerScroll" "on"
+        Option "HorizEdgeScroll" "on"
+        Option "HorizTwoFingerScroll" "on"
+        Option "CircularScrolling" "on"
+        Option "CircScrollTrigger" "2"
+        Option "EmulateTwoFingerMinZ" "40"
+        Option "EmulateTwoFingerMinW" "8"
+        Option "CoastingSpeed" "0"
+        Option "FingerLow" "30"
+        Option "FingerHigh" "50"
+        Option "MaxTapTime" "125"
+EndSection
+EOF
--- void-awesome/scripts/spotx.sh
+++ void-awesome/scripts/spotx.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 set -e
-echo "[*] Install SpotX in Flatpak Spotify"
 
 # https://github.com/SpotX-Official/SpotX-Bash
-bash <(curl -sSL https://spotx-official.github.io/run.sh) -P /var/lib/flatpak/app/com.spotify.Client/x86_64/stable/active/files/extra/share/spotify/
+echo "[*] Install SpotX in Flatpak Spotify"
+
+DIR1=/var/lib/flatpak/app/com.spotify.Client/x86_64/stable/active/files/extra/share/spotify
+DIR2="$HOME/.local/share/flatpak/app/com.spotify.Client/current/active/files/extra/share/spotify"
+
+if [ -e "$DIR1" ]; then
+    DIR="$DIR1"
+else
+    DIR="$DIR2"
+fi
+
+bash <(curl -sSL https://spotx-official.github.io/run.sh) -P "$DIR"
